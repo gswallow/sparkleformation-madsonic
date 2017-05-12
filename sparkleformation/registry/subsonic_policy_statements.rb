51495812a4aff8c5761f6b7917b954c6c303690f
Greg Swallow <gswallow@gmail.com>
2017-05-11 22:59:12 -0400

    Fix git repo, IAM policy.

--- sparkleformation/registry/subsonic_policy_statements.rb
+++ sparkleformation/registry/subsonic_policy_statements.rb
@@ -4,7 +4,11 @@ SfnRegistry.register(:subsonic_policy_statements) do
       'Action' => %w(s3:*),
       'Resource' => join!( join!( 'arn', 'aws', 's3', '', '', ref!(:subsonic_s3_bucket), :options => { :delimiter => ':' }), '*', :options => { :delimiter => '/' }),
       'Effect' => 'Allow'
-
+    },
+    {
+      'Action' =>  %w(s3:ListBucket),
+      'Resource' => join!( 'arn', 'aws', 's3', '', '', ref!(:subsonic_s3_bucket), :options => { :delimiter => ':' })
+      'Effect' =>  'Allow'
     },
     {
       'Action' => %w(cloudformation:DescribeStackResource
--- sparkleformation/subsonic.rb
+++ sparkleformation/subsonic.rb
@@ -5,7 +5,7 @@ EOF
 
   parameters(:ansible_playbook_repo) do
     type 'String'
-    default ENV.fetch('ansible_playbook_repo', 'https://github.com/indigobio/sparkleformation-indigo-empire.git')
+    default ENV.fetch('ansible_playbook_repo', 'https://github.com/gswallow/sparkleformation-subsonic.git')
     allowed_pattern "[\\x20-\\x7E]*"
     description 'Git repository containing ansible playbook'
     constraint_description 'can only contain ASCII characters'
