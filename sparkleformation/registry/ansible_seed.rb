24620e7f11b9794c0baf9e40a5225d43d4d3b96b
Greg Swallow <gswallow@gmail.com>
2017-05-12 12:45:25 -0400

    Add tasks to mount s3 fuse filesystem.

--- sparkleformation/registry/ansible_seed.rb
+++ sparkleformation/registry/ansible_seed.rb
@@ -4,7 +4,9 @@ SfnRegistry.register(:ansible_seed) do |_name, _config = {}|
     'ANSIBLE_REMOTE_TEMP'             => '$HOME/.ansible/tmp',
     'SUBSONIC_VERSION'                => ref!(:subsonic_version),
     'SUBSONIC_LICENSE_KEY'            => ref!(:subsonic_license_key),
-    'SUBSONIC_S3_BUCKET_NAME'         => ref!(:subsonic_s3_bucket_name),
+    'S3FS_BUCKET_NAME'                => ref!(:subsonic_s3_bucket_name),
+    'S3FS_AWS_REGION'                 => region!,
+    'S3FS_IAM_ROLE'                   => ref!(:subsonic_i_a_m_role),
     'HOSTGROUP'                       => 'default'
   }
 end
