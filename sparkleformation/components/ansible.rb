3bac4b75d8f55771b35ffb948a5db8e634e942fb
Greg Swallow <gswallow@gmail.com>
2017-09-22 14:06:25 -0400

    Update ansible (2.3.0.0 is gone)

--- sparkleformation/components/ansible.rb
+++ sparkleformation/components/ansible.rb
@@ -1,7 +1,7 @@
 SparkleFormation.component(:ansible) do
   parameters(:ansible_version) do
     type 'String'
-    default ENV.fetch('ansible_version', '2.3.0.0-1ppa')
+    default ENV.fetch('ansible_version', '2.4.0.0-1ppa')
   end
 
   parameters(:ansible_inventory) do
