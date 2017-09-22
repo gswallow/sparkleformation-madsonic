8aabfb11b1dc1d648e4ad68a372348c15749aec1
Greg Swallow <gswallow@gmail.com>
2017-09-22 11:50:50 -0400

    Hello, Canada and Europe.  Update base AMI.

--- sparkleformation/components/ami.rb
+++ sparkleformation/components/ami.rb
@@ -1,8 +1,12 @@
 SparkleFormation.component(:ami) do
   mappings(:region_to_ami) do
-    set!('us-east-1'.disable_camel!, :ami => 'ami-80861296')
-    set!('us-east-2'.disable_camel!, :ami => 'ami-618fab04')
-    set!('us-west-1'.disable_camel!, :ami => 'ami-2afbde4a')
-    set!('us-west-2'.disable_camel!, :ami => 'ami-efd0428f')
+    set!('us-east-1'.disable_camel!, :ami => 'ami-1d4e7a66')
+    set!('us-east-2'.disable_camel!, :ami => 'ami-dbbd9dbe')
+    set!('us-west-1'.disable_camel!, :ami => 'ami-969ab1f6')
+    set!('us-west-2'.disable_camel!, :ami => 'ami-8803e0f0')
+    set!('ca-central-1'.disable_camel!, :ami => 'ami-a9c27ccd')
+    set!('eu-west-1'.disable_camel!, :ami => 'ami-674cbc1e')
+    set!('eu-west-2'.disable_camel!, :ami => 'ami-03998867')
+    set!('eu-central-1'.disable_camel!, :ami => 'ami-958128fa')
   end
 end
